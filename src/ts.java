659c28847cc1dc664629803c5cbdb1eecc824eb2
jowooseung <champion1824@gmail.com>
2016-03-25 11:02:45 +0900

    git commit

---
+++ src/ts.java
@@ -0,0 +1,15 @@
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by Jo on 2016-03-25.
+ */
+public class ts {
+
+    public static void main (String [] args) {
+
+        String name = null;
+
+        System.out.println(StringUtils.isEmpty(name));
+        System.out.println("test");
+    }
+}
